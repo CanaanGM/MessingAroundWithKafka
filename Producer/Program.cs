ed37523954d6b6c42cb63916986758fb1124285f
Canaan <reakak1@gmail.com>
2022-09-03 02:59:02 +0300

    Producer

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+using Newtonsoft.Json;
+
+using Shared;
+
+using System.Threading;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+
+    string? state;
+
+    while ((state = Console.ReadLine()) != null)
+    {
+        var response =
+            await producer.ProduceAsync(
+                "Kefka", new Message<Null, string>
+                {
+                    Value = JsonConvert.SerializeObject(new Monster(11, 21, 13, 9, "Slime"))
+                }
+            );
+
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
