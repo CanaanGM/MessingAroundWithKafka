4f9ad959e7df371975f4006f4444a2cab919a5f5
Canaan <reakak1@gmail.com>
2022-09-03 03:01:01 +0300

    shared classes

---
+++ Shared/Monster.cs
@@ -0,0 +1,25 @@
+﻿namespace Shared
+{
+    public class Monster
+    {
+        public int Inti { get; set; }
+        public int Vitality { get; set; }
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public string Name { get; set; }
+
+        public Monster()
+        {
+
+        }
+
+        public Monster(int inti, int vitality, int strength, int agility, string name)
+        {
+            Inti = inti;
+            Vitality = vitality;
+            Strength = strength;
+            Agility = agility;
+            Name = name;
+        }
+    }
+}
